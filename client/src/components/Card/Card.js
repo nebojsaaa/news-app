2ceec73c3fef1edcc5f823746f0be60780a52f30
Nebojsa Jovanovic <n.jovanovic@vegaitsourcing.rs>
2019-08-22 16:37:54 +0200

    finish responsive design for Card and CardList components

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -7,11 +7,9 @@ const Card = ({ article }) => {
 			<div className="card__img">
 				<img src={article.urlToImage} alt="alt"/>
 			</div>
-			<div className="card__body">
-				<h5 className="card__title">{article.title}</h5>
-				<p className="card__text">{article.description}</p>
-				<button className="card__btn">More</button>
-			</div>
+			<h5 className="card__title">{article.title}</h5>
+			<p className="card__text">{article.description}</p>
+			<button className="card__btn">More</button>
 		</div>
 	);
 }
